e847c1e7e557cf8eae41c44d83f954e72be708d8
Welington Panzarini <121058195+WelingtonPanzarini@users.noreply.github.com>
2023-05-03 14:20:04 -0300

    urna

--- etecEstudosPw2/urnaVotacao/script.js
+++ etecEstudosPw2/urnaVotacao/script.js
@@ -1,24 +1,29 @@
 function inserir(valor) {
+    //recebe os valores dos campos 1 e 2
     var valor1 = document.getElementById("campo1").value;
     var valor2 = document.getElementById("campo2").value;
-
+    //se valor1 vazio receba o valor do campo1
     if (valor1 == "") {
         document.getElementById("campo1").value = valor;
+    //senão vá para o valor2 e receba valor do campo2
     } else if (valor2 == "") {
         document.getElementById("campo2").value = valor;
     }
 }
 
 function corrige() {
+    //corrige o valor dos campos 1 e 2 com vazio
     document.getElementById("campo1").value = "";
     document.getElementById("campo2").value = "";
 }
 
 function votar() {
+    //valores 1 e 2 recebem os valores respectivos dos campos 1 e 2
     var valor1 = parseInt(document.getElementById("campo1").value);
     var valor2 = parseInt(document.getElementById("campo2").value);
    
     var candidato = (valor1 * 10) + valor2;
+    //valor1 X 10 garante a casa decimal + valor2
     if (sessionStorage.getItem(candidato) !== null) {
         votos = parseInt(sessionStorage.getItem(candidato)) + 1;
         sessionStorage.setItem(candidato, votos);   
@@ -30,23 +35,31 @@ function votar() {
     document.getElementById("campo2").value = "";
     
 }
-function resultado( ) {
+function resultado() {
     document.getElementById("resultado").innerHTML = ""
     for (i = 0; i < 100; i++) {
         if (sessionStorage.getItem(i) !== null) {
             document.getElementById("resultado").innerHTML += "Candidato  " + i + " tem "
-                + sessionStorage.getItem(i) + " votos<br/>"       
+                + sessionStorage.getItem(i) + " votos<br/>" //.innerHTML=innerHTML+"Candidato"+i+...
+                //Aqui limpa os votos computados 
+                //sessionStorage.clear("votos");     
 
         }
     }
-
 }
-/*function branco() {
-    document.getElementById("resultado").innerHTML = ""
-    for (i = 0; i < 100; i++) {
-        if (sessionStorage.getItem(i) !== null) {
-            document.getElementById("resultado").innerHTML += "Branco" + i + "tem"
-                + sessionStorage.getItem(i) + "votos<br/>"
-        }
+function branco() {
+    var valor1 = parseInt(document.getElementById("campo1").value);
+    var valor2 = parseInt(document.getElementById("campo2").value);    
+   
+    var nulo = (valor1 * 10) + valor2;
+    if (sessionStorage.getItem(nulo) !== null) {
+        nulo = parseInt(sessionStorage.getItem(nulo)) + 1;
+        sessionStorage.setItem(nulo, nulo);   
+    } else {
+        sessionStorage.setItem(nulo, 1);
     }
-}*/
+    alert("Confirmado voto em Branco ")
+    document.getElementById("campo1").value = " ";
+    document.getElementById("campo2").value = " "; 
+    
+}
